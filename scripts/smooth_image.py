06c837a13ec0f5a04ce00d037dc8467ee5040c68
acse-0e251b1d <ym722@ic.ac.uk>
2022-10-10 18:46:00 +0100

    afternoon

--- scripts/smooth_image.py
+++ scripts/smooth_image.py
@@ -1,5 +1,26 @@
+from envtest import smooth_image
 from envtest import rand_array
+from scipy import misc
+import numpy as np
+from scipy.ndimage import gaussian_filter
+from scipy import misc
+import matplotlib.pyplot as plt
 
 shape = (3, 3)
 
+image = misc.ascent()
+sigma = 5
+smoothed_image = smooth_image(image,sigma)
+
+f = plt.figure()
+f.add_subplot(1,2,1)
+plt.imshow(image)
+f.add_subplot(1,2,2)
+plt.imshow(smoothed_image)
+plt.show(block=True)
+
 print(rand_array(shape))
+
+
+def smooth_image(a,sigma=1):
+    return gaussian_filter(a,sigma=sigma)
